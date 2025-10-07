521000a9a9b5cc5b4bd15a8c80c6dad1598981b1
tkhs-0114 <tkhs.mail0114@gmail.com>
2025-10-07 16:45:32 +0900

    sample2-2

--- src/main/java/oit/is/group7/calculation/controller/Sample21Controller.java
+++ src/main/java/oit/is/group7/calculation/controller/Sample21Controller.java
@@ -3,10 +3,22 @@ package oit.is.group7.calculation.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+
 @Controller
 public class Sample21Controller {
   @GetMapping("/sample21")
   public String sample21() {
     return "sample21";
+  }
+
+  @GetMapping("/sample22/{param1}/{param2}")
+  public String sample22(@PathVariable String param1, @PathVariable String param2, ModelMap model) {
+    int a = Integer.parseInt(param1);
+    int b = Integer.parseInt(param2);
+    int sum = a + b;
+    model.addAttribute("sum", sum);
+    return "sample21.html";
   }
 }
