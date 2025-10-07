40315f0f5abb7018df9d594bafff8752acf5af70
tkhs-0114 <tkhs.mail0114@gmail.com>
2025-10-07 15:18:31 +0900

    init

---
+++ src/test/java/oit/is/group7/calculation/CalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.group7.calculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
